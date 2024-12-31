64c78b723e62a7b5cbe2080907c65ee0eab5bce0
yakarasi <harisamaruki1009@gmail.com>
2025-01-01 04:02:03 +0900

    テストゲームビルド

---
+++ Assets/CsScripts/DragTest.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragTest : MonoBehaviour
+{
+    float objPosZ;
+    void Start()
+    {
+        objPosZ = transform.position.z;  
+        Debug.Log(objPosZ);     
+    }
+
+   void OnMouseDrag()
+    {
+        Vector3 objPos = Camera.main.WorldToScreenPoint(transform.position);
+
+        Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, objPos.z);
+
+        transform.position = Camera.main.ScreenToWorldPoint(mousePos);
+    }
+    void Update()
+    {
+        
+    }
+}
