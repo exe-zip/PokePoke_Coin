7b6241c85c1e4840ae5214e9cf3f3641dc815620
yakarasi <harisamaruki1009@gmail.com>
2025-01-01 20:38:05 +0900

    モデル置き換え
    カメラを並行投影に変更、プログラムを修正

--- Assets/CsScripts/CoinThrow.cs
+++ Assets/CsScripts/CoinThrow.cs
@@ -11,32 +11,50 @@ public class CoinThrow : MonoBehaviour
     Vector3 throwForce;
     Vector3 torqueVec;
     Vector3 torqueForce;
-    [SerializeField]
+    Vector3 defPos;
+    Vector3 objPos;
+    Quaternion defRot;
+    float upForce;
     float forceDebuff;
     float torqueBuff;
+
+    void ResetTF(){
+        transform.position = defPos;
+        transform.rotation = defRot;
+    }
     void Start()
     {
         rigit = GetComponent<Rigidbody>();
+        rigit.maxAngularVelocity = 1000f;
+        
+
+        defPos = transform.position;
+        defRot = transform.rotation;
+        
         rigit.isKinematic = true;
-        forceDebuff = 0.4f;
-        torqueBuff = 10f;
+
+        forceDebuff = 0.0003f;
+        upForce = 0.008f;
     }
 
     void OnMouseDown(){
         rigit.isKinematic = true;
 
+        torqueBuff = Random.Range(2f,4f);
+
         startPoint = Input.mousePosition;
+
+        objPos = Camera.main.WorldToScreenPoint(transform.position);
     }
     void OnMouseDrag()
     {
-        Vector3 objPos = Camera.main.WorldToScreenPoint(transform.position);
         Vector3 mousePos = new Vector3(Input.mousePosition.x, Input.mousePosition.y, objPos.z);
         
         transform.position = Camera.main.ScreenToWorldPoint(mousePos);
 
         deltaPoint = Input.mousePosition;
         throwVec = (deltaPoint - startPoint) * forceDebuff;
-        throwForce = new Vector3(throwVec.x, throwVec.y, throwVec.y);
+        throwForce = new Vector3(throwVec.x, upForce, throwVec.y);
 
         torqueVec = (deltaPoint - startPoint) * torqueBuff;
         torqueForce = new Vector3(torqueVec.y, torqueVec.x, torqueVec.z);
@@ -44,13 +62,20 @@ public class CoinThrow : MonoBehaviour
         startPoint = Input.mousePosition;
     }
     void OnMouseUp(){
-        rigit.isKinematic = false;
-
-        rigit.AddForce(throwForce, ForceMode.Impulse);
-        rigit.AddTorque(torqueForce, ForceMode.Acceleration);
+        Debug.Log(throwForce.y);
+        if(throwForce.z > 0.0015f){
+            rigit.isKinematic = false;
+            rigit.AddForce(throwForce, ForceMode.Impulse);
+            rigit.AddTorque(torqueForce, ForceMode.VelocityChange);
+        }
+        else{
+            ResetTF();
+        }
     }
     void Update()
     {
-        
+        if(transform.position.y < -1f){
+            ResetTF();
+        }
     }
 }
